39ef3a1454617207b0e11975bb1a8c436dad5d0c
PesHwA07 <naiknawaredhananjay015@gmail.com>
2026-07-23 13:35:38 +0530

    Finalize integration between Airflow tasks and Postgres

--- dags/spotify_dag.py
+++ dags/spotify_dag.py
@@ -8,3 +8,20 @@ from spotify_etl import extract_spotify_data, transform_and_load_spotify_data
 default_args = {
     'owner': 'data_engineer',
     'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=5),
+}
+
+with DAG(
+    'spotify_etl_pipeline',
+    default_args=default_args,
+    description='Daily ETL pipeline for Spotify Tracks dataset',
+    schedule_interval='@daily',
+    start_date=datetime(2026, 7, 1),
+    catchup=False,
+    tags=['spotify', 'etl', 'batch'],
+) as dag:
+
+    extract_task = PythonOperator(
